f89f78610e0283d7bd34c5c122a053e82e136781
JtakyLR <68268530+JtakyLR@users.noreply.github.com>
2025-05-23 21:27:15 +0700

    Update check_reg.sh
    Fix

--- Check_domain_and_certificate/check_reg.sh
+++ Check_domain_and_certificate/check_reg.sh
@@ -8,6 +8,29 @@ fun_error_reg_datepaid(){
 fun_error_reg_daysleft(){
 	echo "Error! not found - reg_daysleft"
 }
+
+fun_extract_date() {
+  local line="$1"
+  local date=""
+
+  #YYYY-MM-DD
+   if [[ "$line" =~ ([0-9]{4})-([0-9]{2})-([0-9]{2}) ]]; then
+    date="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}-${BASH_REMATCH[3]}"
+
+  #DD-MM-YYYY
+  elif [[ "$line" =~ ([0-9]{2})-([0-9]{2})-([0-9]{4}) ]]; then
+    date="${BASH_REMATCH[3]}-${BASH_REMATCH[2]}-${BASH_REMATCH[1]}"
+  fi
+
+  echo "$date"
+}
+fun_expiry_line() {
+  local domain="$1"
+
+  whois -H "$domain" | grep -Ei \
+    "Expiry|Registry Expiry Date|paid-till|expires|Expiry Date|Record expires|Expiration Time|Registry Expiry Date"
+}
+
 REG_DOMAINS=(/usr/lib/zabbix/externalscripts/check_domains/reg/reg_domains)
 REG_CHECK=(/usr/lib/zabbix/externalscripts/check_domains/reg/reg_datepaid)
 
@@ -19,19 +42,20 @@ then
 		reg_datepaid)
 			if [ $# -eq 2 ]
 			then
-				DATEPAID=$(whois -I ${2} | grep -e "Registry Expiry Date" -e "paid-till" -e "expires" | cut -d ":" -f 2 | sed 's/^ *//' | cut -d " " -f 1 |sed -r 's/T.+//')
-#				DATEPAID=$()
-				if [ -z ${DATEPAID} ]; then
-					DATEPAID=$(cat ${REG_CHECK}| grep ${2} | cut -d ":" -f 2)
-					echo ${DATEPAID}
+				GETDATE=$(fun_expiry_line "$2")
+				CORDATE=$(fun_extract_date "${GETDATE}")
+
+				if [ -z ${CORDATE} ]; then
+					CORDATE=$(cat ${REG_CHECK}| grep ${2} | cut -d ":" -f 2)
+					echo ${CORDATE}
 				else
 					if grep -q ${2} "$REG_CHECK"; then
 						OLD_DELETE=$(sed -i '/'${2}'/d' ${REG_CHECK})
-						echo ${DATEPAID}
-						echo ${2}:${DATEPAID} >> ${REG_CHECK}
+						echo ${CORDATE}
+						echo ${2}:${CORDATE} >> ${REG_CHECK}
 					else
-						echo ${DATEPAID}
-                                                echo ${2}:${DATEPAID} >> ${REG_CHECK}
+						echo ${CORDATE}
+                                                echo ${2}:${CORDATE} >> ${REG_CHECK}
 					fi
 				fi
 			else
